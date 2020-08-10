6f71a90bd327304fc4250ebac299cbb62ee3982e
Darrel Kathan <darrel.kathan@grainger.com>
2020-08-10 18:21:50 -0500

    First commit

---
+++ .circleci/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+current_function="$1"
+current_build="${current_function}_${DEPLOY_ENVIRONMENT}"
+cd $current_function
+
+if [ -f package-lock.json ]; then
+    npm ci
+else
