7291e0b46b924cd8a45a5af2a7278a23adffed17
Darrel Kathan <darrel.kathan@grainger.com>
2020-08-25 14:57:48 -0500

    Added proxy to API

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports.handler = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
