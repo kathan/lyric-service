b60c843811224396316ff0d22a5614500ed67f18
Darrel Kathan <darrel.kathan@grainger.com>
2020-08-26 13:41:09 -0500

    Added status code restrictions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const dispatch = require('lib/dispatch.js')
+const statusCodes = require('lib/statusCodes.js')
 const response = {
   "statusCode": 200,
   "isBase64Encoded": false
@@ -8,7 +9,7 @@ const response = {
 module.exports.handler = async request => {
   await dispatch(request, response)
   return {
-    statusCode: response.statusCode,
+    statusCode: statusCodes.includes(response.statusCode) ? response.statusCode : 500,
     body: response.body,
   };
 };
