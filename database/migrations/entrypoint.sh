0df484628602ed835f2e6b33fd93ceda3eb33fa1
Darrel Kathan <darrel.kathan@grainger.com>
2020-09-10 11:28:07 -0500

    Added flyway env variables to flyway.conf

--- database/migrations/entrypoint.sh
+++ database/migrations/entrypoint.sh
@@ -4,6 +4,6 @@ set -o errexit -o pipefail -x
 
 FLYWAY_COMMANDS="info migrate info"
 
-echo "Flyway url: $FLYWAY_URL"
+echo "Flyway url: $FU"
 echo "Running migrations"
 flyway -configFiles=database/migrations/flyway.conf -locations=filesystem://migrations/sql $FLYWAY_COMMANDS
