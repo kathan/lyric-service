f831cbc1b3de4f81cf9469ab9ba071dde5de780c
Darrel Kathan <darrel.kathan@grainger.com>
2020-09-10 11:17:45 -0500

    Add migrations/entrypoint.sh

--- database/migrations/entrypoint.sh
+++ database/migrations/entrypoint.sh
@@ -4,5 +4,6 @@ set -o errexit -o pipefail -x
 
 FLYWAY_COMMANDS="info migrate info"
 
+echo "Flyway url: $FLYWAY_URL"
 echo "Running migrations"
 flyway -configFiles=database/migrations/flyway.conf -locations=filesystem://migrations/sql $FLYWAY_COMMANDS
