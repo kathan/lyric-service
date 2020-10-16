30c50add640e1a1c393d05249e2a9cff5f0f961a
Darrel Kathan <darrel.kathan@grainger.com>
2020-10-16 11:58:42 -0500

    Moved default response to local handler

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const dispatch = require('./lib/dispatch.js')
 const statusCodes = require('./lib/statusCodes.js')
 
 module.exports.handler = async request => {
+  const response = {
+    "statusCode": 200,
+    "isBase64Encoded": false,
+  }
+  
   await dispatch(request, response)
   return {
     statusCode: statusCodes.includes(response.statusCode) ? response.statusCode : 500,
