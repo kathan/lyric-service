d87bd45a0db11181b8f660635fb8bf06cb8c1288
Darrel Kathan <darrel.kathan@grainger.com>
2020-11-03 15:43:47 -0600

    Added src/lib/HandlerInterface.js

---
+++ src/lib/HandlerInterface.js
@@ -0,0 +1,27 @@
+class HandlerInterface{
+
+    async get (request, response){
+        response.statusCode = 405
+    }
+
+    async put (request, response){
+        response.statusCode = 405
+    }
+
+    async post (request, response){
+        response.statusCode = 405
+    }
+
+    async patch (request, response){
+        response.statusCode = 405
+    }
+
+    async delete (request, response){
+        response.statusCode = 405
+    }
+
+    async before (request, response){
+    }
+}
+
+module.exports = HandlerInterface
