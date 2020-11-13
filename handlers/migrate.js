e76961563613b5f1913ddc2647400c46dbc5e9c3
Darrel Kathan <darrel.kathan@grainger.com>
2020-11-13 09:33:55 -0600

    Attempting to get in-memory flag for migrations

--- handlers/migrate.js
+++ handlers/migrate.js
@@ -1,9 +1,16 @@
 const HandlerInterface = require("../lib/HandlerInterface")
 const db = require('../lib/dbConnection.js')
+let current = false
 
 class Migrate extends HandlerInterface{
     async post(request, response){
         await db.sync({alter: true})
+        response.statusCode = 200
+        current = true
+    }
+
+    async get(request, response){
+        response.body.current = current
         response.statusCode = 200
     }
 }
