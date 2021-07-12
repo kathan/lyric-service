914e0dd15153dfea698ff5023c467830d9ee3980
kathan <darrel.kathan@grainger.com>
2021-07-12 18:32:37 -0500

    Removed migration from circle

---
+++ handlers/migrate.js
@@ -0,0 +1,22 @@
+const HandlerInterface = require("../lib/HandlerInterface");
+const db = require('../lib/dbConnection');
+
+class Migrate extends HandlerInterface{
+    constructor(){
+        super();
+    }
+
+    async post(request, response){
+        try{
+            await db.sync({alter: true});
+            response.statusCode = 200;
+        }catch(err){
+            const msg = `Error in migrate handler: ${err}`;
+            console.error(msg);
+            response.body = msg;
+            response.statusCode = 500;
+        }
+    }
+}
+
+module.exports = Migrate; 
